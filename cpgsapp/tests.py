ec10c3d441fcf8c4d7d9c4df72f0e357ab80dc86
trio <amal.benny@liquidlab.in>
2025-02-25 16:11:05 +0530

    Add files via upload

--- cpgsapp/tests.py
+++ cpgsapp/tests.py
@@ -1,19 +1,30 @@
-import asyncio
-import threading
-import time
+import cv2
+import numpy as np
+from paddleocr import PaddleOCR
 
+# Load image
 
-def capture():
+from picamera2 import Picamera2
+cap = Picamera2()
+cap.start()
 
-        while True:
-            time.sleep(2)
-            print("Capturing")
+frame = cap.capture_array()
+# # Convert to grayscale
+# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+# # Apply Gaussian blur
+# blurred = cv2.GaussianBlur(gray, (5,5), 0)
 
-Thread = threading.Thread(target=capture)
-Thread.start()
+# # Apply Adaptive Thresholding
+# thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 
+# # Save processed image and run OCR
+# cv2.imwrite("processed_car.jpg", thresh)
 while True:
+    ocr = PaddleOCR(use_angle_cls=True, use_gpu=False)
+    results = ocr.ocr(frame, cls=True)
+    
+    # Print results
+    for line in results[0]:
+        print(line[1][0])  # Extracted text
 
-    time.sleep(2)
-    print('hello')
