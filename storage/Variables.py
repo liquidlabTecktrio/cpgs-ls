db4a257299a8a7b82f4b5c69015b42974c4f3428
trio <amal.benny@liquidlab.in>
2025-02-27 12:09:14 +0530

    Add files via upload

---
+++ storage/Variables.py
@@ -0,0 +1,10 @@
+
+points = []
+coordinates = []
+cap = None
+licensePlate = ""
+SPACES = []
+licensePlateinSpace =""
+licensePlateinSpaceInBase64 = ""
+licensePlateBase64 = ""
+space_coordinate_list = []
