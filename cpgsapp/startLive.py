84b8ff9b060e9a9f3e15c6b76503c02b9f27d44f
trio <amal.benny@liquidlab.in>
2025-03-14 16:37:39 +0530

    Add files via upload

--- cpgsapp/startLive.py
+++ cpgsapp/startLive.py
@@ -14,14 +14,12 @@ import requests
 """Wait until the server is listening on the specified port."""
 while True:
     try:
-
-
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.settimeout(1)
             s.connect(("0.0.0.0", 8000))
-        print(f"Server is up on 0.0.0.0:8000")
-        requests.get(f'http://0.0.0.0:8000/initiate')
-        break
+            print(f"Server is up on 0.0.0.0:8000")
+            requests.get(f'http://0.0.0.0:8000/initiate')
+            break
     except (ConnectionRefusedError, socket.timeout):
         print(f"Waiting for server on 0.0.0.0:8000...")
         time.sleep(1)  # Check every second
